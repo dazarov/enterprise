9e01387ab46c1c4c294120b41333037c06eb696c
Daniel Azarov <daniel@exadel.com>
2016-09-29 15:29:43 -0700

    PhotoBlog
    Loggable annotation updated

--- plugins/blog/blog/src/main/java/com/daniel/blog/annotations/Loggable.java
+++ plugins/blog/blog/src/main/java/com/daniel/blog/annotations/Loggable.java
@@ -1,5 +1,12 @@
 package com.daniel.blog.annotations;
 
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Target( ElementType.METHOD )
+@Retention( RetentionPolicy.RUNTIME ) // default retention
 public @interface Loggable {
 
 }
