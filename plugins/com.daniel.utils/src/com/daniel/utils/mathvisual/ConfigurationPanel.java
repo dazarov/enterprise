70ea894c8af86aec796423869580a14a0c9eab5e
Daniel Azarov <daniel@exadel.com>
2018-04-15 21:16:07 -0700

    exp4 library added

--- plugins/com.daniel.utils/src/com/daniel/utils/mathvisual/ConfigurationPanel.java
+++ plugins/com.daniel.utils/src/com/daniel/utils/mathvisual/ConfigurationPanel.java
@@ -8,5 +8,9 @@ public class ConfigurationPanel extends JPanel{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	Expression calc = new ExpressionBuilder("3 * sin(y) - 2 / (x - 2)")
+		    .variable("x", x)
+		    .variable("y", y)
+		    .build();
+		double result1 = calc.evaluate();
 }
